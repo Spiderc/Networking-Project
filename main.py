306df96cecfe5fd4db294bf46e1b82e6e3f40748
Andrew Schleicher <spiderc126@yahoo.com>
2015-04-13 20:09:19 -0500

    Made while true a function
    made the while(true) loop a function for later threading

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def getState():
 		result = "join"
 	return result
 
-	
-ids = id.Id()
-while True:
-	state = getState()
-	if state == "join":
-		pass
-		#do join stuff here
-	elif state == "participate":
-		pass
-		#check to see if there's any datagrams in the outgoing queue
-		#else check to see if there's any datagrams in the incoming queue
-		if not ids.idQueue.full(): #make this an elif once the others parts are implemented
-			ids.generateId()
+def mainLoop():
+	ids = id.Id()
+	while True:
+		state = getState()
+		if state == "join":
+			pass
+			#do join stuff here
+		elif state == "participate":
+			pass
+			#check to see if there's any datagrams in the outgoing queue
+			#else check to see if there's any datagrams in the incoming queue
+			if not ids.idQueue.full(): #make this an elif once the others parts are implemented
+				ids.generateId()
