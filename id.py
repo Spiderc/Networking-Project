a7df98f315834730a23a5212c0bd8d48530849ea
Andrew Schleicher <spiderc126@yahoo.com>
2015-04-16 15:02:01 -0500

    Improved Id
    made a instance id bytearray
    made functions that return the instance bytearray as hex or a string

--- id.py
+++ id.py
@@ -5,31 +5,36 @@ class Id:
 	idLengthInBytes = 16
 	maxQueueLength = 10000
 	idQueue = Queue.Queue(maxQueueLength)
-	zeroId = "0" * idLengthInBytes
+	zeroId = bytearray(idLengthInBytes)
 
 	def __init__(self):
-		pass
+		self.id = bytearray(os.urandom(Id.idLengthInBytes))
 
-	def generateId(self):
+	def idFactory(self):
 		if Id.idQueue.qsize() < Id.maxQueueLength:
 			Id.idQueue.put(os.urandom(Id.idLengthInBytes))
 		
-	def getId(self):
+	def generateId(self):
 		if Id.idQueue.empty():
-			return os.urandom(Id.idLengthInBytes)
+			self.id = os.urandom(Id.idLengthInBytes)
 		else:
-			return Id.idQueue.get();
+			self.id = Id.idQueue.get();
 
 	def setIdLength(self,lengthInBytes):
 		if lengthInBytes != Id.idLengthInBytes:
 			Id.idLengthInBytes = lengthInBytes
 			Id.zeroId = "0" * lengthInBytes
-			#clear out the queue
+			while Id.idQueue.qsize() > 0:
+				Id.idQueue.get() #probably a better way of doing this
+			self.generateId()
 
 	def setMaxQueueLength(self,length):
 		Id.maxQueueLength = length
 		while Id.idQueue.qsize() > length:
 			Id.idQueue.get() #probably a better way of doing this
 	
-	def getIdString(self):
-		return self.getId().encode("hex")
+	def getAsHex(self):
+		return self.getAsString().encode("hex")
+		
+	def getAsString(self):
+		return str(self.id)
