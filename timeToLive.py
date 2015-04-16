f310b70ae70dc80c532db6c7c61cbf01fffb42d9
froe0466 <jamesfroehling@hotmail.com>
2015-04-16 14:28:57 -0500

    timeToLive Class
    This is the first pass at the timeToLive class

---
+++ timeToLive.py
@@ -0,0 +1,17 @@
+
+
+class timeToLive:
+
+	timeToLive
+	
+	def TimeToLive(timeToLive):
+		
+		set(timeToLive)
+		
+	def get():
+		
+		return timeToLive
+		
+	def set(timeToLive):
+	
+		this.timeToLive = timeToLive
