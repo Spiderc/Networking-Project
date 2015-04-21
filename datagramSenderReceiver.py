b5624e1af7c3407de316534699d11cdc557a9479
froe0466 <jamesfroehling@hotmail.com>
2015-04-21 15:00:24 -0500

    datagramSenderReveiver Comments
    Added comments to how the sender and receiver work

--- datagramSenderReceiver.py
+++ datagramSenderReceiver.py
@@ -15,24 +15,33 @@ class datagramSenderReceiver:
 			
 	def receive(self):
 		#look for incoming traffic, put in queue
-			
+		
+		#Create a new socket (INET Sockets, Datagram Packets, UDP)
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+		#Set socket options - Prevent TIME_WAIT state inorder to reuse the address
 		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		sock.bind(('', datagramSocket))
+		#Bind socket to IP and Port
+		sock.bind((multicastGroup, datagramSocket))
+		
+		#??MemberShip request packet
 		mreq = struct.pack("4sl", socket.inet_aton(multicastGroup), socket.INADDR_ANY)
-
 		sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 		#Put in incomming queue
 		#buffer sizze of 10,240 for incoming data
-		receiveQueue.put(sock.recv(10240))
+		while(true) #Determine length of the loop
+			receiveQueue.put(sock.recv(10240)) #need to look at this, going to string?
 	  
 	def send(self,datagramPacket):
-		#send packet
-				
-			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-			sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-			sock.sendto("robot", (multicastGroup, datagramSocket))
+					
+		#Create a new socket (INET Sockets, Datagram Packets, UDP)			
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+		
+		#Set socket options - TimeToLive??
+		sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+		
+		#multicast send the datagramPacket
+		sock.sendto(datagramPacket, (multicastGroup, datagramSocket))
 		
 		
 		
