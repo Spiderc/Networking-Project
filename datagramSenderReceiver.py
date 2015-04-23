592bde9550860fd3c9f72e058d9c4c050ad1e970
froe0466 <jamesfroehling@hotmail.com>
2015-04-23 14:32:09 -0500

    datagramSenderReceiver Working Test Update
    Edited IP address for sending / receiving and was able to send and
    receive a hello world string

--- datagramSenderReceiver.py
+++ datagramSenderReceiver.py
@@ -3,7 +3,7 @@ import struct
 
 class datagramSenderReceiver:
 	datagramSocket = 12345
-	multicastGroup = '224.1.1.1'
+	multicastGroup = '140.209.121.187'
 
 	def __init__(self, sendQueue, receiveQueue):
 		self.sendQueue = sendQueue
@@ -14,6 +14,8 @@ class datagramSenderReceiver:
 		pass
 			
 	def receive(self):
+		x = 0
+	
 		#look for incoming traffic, put in queue
 		
 		#Create a new socket (INET Sockets, Datagram Packets, UDP)
@@ -21,16 +23,19 @@ class datagramSenderReceiver:
 		#Set socket options - Prevent TIME_WAIT state inorder to reuse the address
 		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		#Bind socket to IP and Port
-		sock.bind((multicastGroup, datagramSocket))
+		sock.bind((datagramSenderReceiver.multicastGroup, datagramSenderReceiver.datagramSocket))
 		
-		#??MemberShip request packet
-		mreq = struct.pack("4sl", socket.inet_aton(multicastGroup), socket.INADDR_ANY)
-		sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-
 		#Put in incomming queue
+		#Determine Loop Size
+		#receiveQueue.put(sock.recv(10240)) #need to look at this, going to string?
+		
 		#buffer sizze of 10,240 for incoming data
-		while(true) #Determine length of the loop
-			receiveQueue.put(sock.recv(10240)) #need to look at this, going to string?
+		#while(true) #Determine length of the loop
+		while x < 10000:
+			print sock.recv(10240)
+			x = x + 1
+		
+		
 	  
 	def send(self,datagramPacket):
 					
@@ -41,7 +46,10 @@ class datagramSenderReceiver:
 		sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 		
 		#multicast send the datagramPacket
-		sock.sendto(datagramPacket, (multicastGroup, datagramSocket))
+		     ###sock.sendto(datagramPacket, (datagramSenderReceiver.multicastGroup, datagramSenderReceiver.datagramSocket))
+		
+		#TEST CODE for sending HI
+		sock.sendto("Hi", (datagramSenderReceiver.multicastGroup, datagramSenderReceiver.datagramSocket))
 		
 		
 		
