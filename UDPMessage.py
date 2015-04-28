02f4894cfd3c79316317605bf2e5072f0ca7c0c3
froe0466 <jamesfroehling@hotmail.com>
2015-04-28 14:56:34 -0500

    UPDMessage Includes byteArray Constructor

--- UDPMessage.py
+++ UDPMessage.py
@@ -2,18 +2,32 @@
 
 class UDPMessage:
 	
-	def __init__(self, id1, id2, timeToLive, message):
-		self.id1 = id1
-		self.id2 = id2
-		self.ttl = timeToLive
-		if message.length > 476:
-			#WRITE ERROR MESSAGE
-		elif message.length < 476:
-			#PADD THE END OF THE MESSAGE TO 476
-		self.message = message
+	def __init__(self, id1=None, id2=None, timeToLive=None, message=None, byteArray=None):
+		
+		if byteArray = None:
+			self.id1 = id1
+			self.id2 = id2
+			self.ttl = timeToLive
+			if message.length > 476:
+				print "ERROR: Andrew sucks and your message is to long"
+				pass
+				#Send Error
+			elif message.length < 476:
+				while message.length < 476:
+					#Pad Message
+					message = message + "|"
+					
+			self.message = message
+			
+		else:
+			self.byteArray = byteArray
 		
 	def getDataGramPacket(self):
 	
-		dataGramPacket = self.id1 + self.id2 + self.ttl + self.message
+		if self.id1 != None:
+			dataGramPacket = self.id1 + self.id2 + self.ttl + self.message
+			
+		else:
+			dataGramPacket = byteArray
 	
 		return dataGramPacket
