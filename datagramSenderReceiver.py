328c86e441c81c18c5021109ea1c20444767e35d
froe0466 <jamesfroehling@hotmail.com>
2015-05-10 18:03:26 -0500

    Added Multicast Receive to DatagramSender

--- datagramSenderReceiver.py
+++ datagramSenderReceiver.py
@@ -1,44 +1,53 @@
 import socket
 import struct
+import multicast
 
 class DatagramSenderReceiver:
-	datagramSocket = 12345
-	multicastGroup = '140.209.121.187'
 
-	def __init__(self, receiveQueue):
+	def __init__(self, receiveQueue, multicastQueue):
 		self.receiveQueue = receiveQueue
+		self.multicastQueue = multicastQueue
 			
-	def receive(self):
+	def receive(self, ipAddress, datagramSocket):
 		x = 0
-		#look for incoming traffic, put in queue
 		
 		#Create a new socket (INET Sockets, Datagram Packets, UDP)
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 		#Set socket options - Prevent TIME_WAIT state inorder to reuse the address
 		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		#Bind socket to IP and Port
-		sock.bind((datagramSenderReceiver.multicastGroup, datagramSenderReceiver.datagramSocket))
-		
-		#Put in incomming queue
-		#Determine Loop Size
+		sock.bind((ipAddress, datagramSocket))
+				
+		#Put in incoming queue
 		while x < 1000:
-			receiveQueue.put(sock.recv(10240)) #need to look at this, going to string?
+			datagramSenderReceiver.receiveQueue.put(sock.recv(10240)) #need to look at this, going to string?
 			##TEST CODE print sock.recv(10240)
+			x = x - 1;
 		
-	def send(self,datagramPacket):
+	def send(self, ipAddress, datagramSocket, datagramPacket):
 		#Create a new socket (INET Sockets, Datagram Packets, UDP)			
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 		
-		#Set socket options - TimeToLive??
-		sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+		#Set socket options - TimeToLive?? - TEST TO MAKE SURE IT CAN BE REMOVED
+		##sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 		
 		#multicast send the datagramPacket
-		sock.sendto(datagramPacket, (datagramSenderReceiver.multicastGroup, datagramSenderReceiver.datagramSocket))
+		sock.sendto(datagramPacket, (ipAddress, datagramSocket))
 		
 		#TEST CODE for sending HI
-		#####sock.sendto("Hi", (datagramSenderReceiver.multicastGroup, datagramSenderReceiver.datagramSocket))
+		#####sock.sendto("Hi", (datagramSenderReceiver.multicastGroup, datagramSenderReceiver.datagramSocket))	
 		
-	def sendToPeer(self, datagramPacket, peerIP):
-		pass #TODO: fix this
-	
+	def multicastReceive(self, ipAddress, datagramSocket):
+		#Create a new socket (INET Sockets, Datagram Packets, UDP)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+		#Set socket options - Prevent TIME_WAIT state inorder to reuse the address
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		#Bind socket to IP and Port
+		sock.bind((ipAddress, datagramSocket))
 		
+		mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+		sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+				
+		#Put in incoming queue
+		while True:
+			datagramSenderReceiver.multicastQueue.put(sock.recv(10240))
