d139d4ce07e9f774332c484b5a58f5e97e165dd4
spiderc <spiderc126@yahoo.com>
2015-05-10 17:55:08 -0500

    Checked TTL

--- timeToLive.py
+++ timeToLive.py
@@ -5,7 +5,7 @@ class TimeToLive:
 
 	def __init__(self, timeToLive=None):		
 		if timeToLive == None:
-			self.ttl = (random.getrandbits(4) + 10) #TODO: test this
+			self.ttl = (random.getrandbits(4) + 10)
 		else:
 			self.ttl = timeToLive
 		
